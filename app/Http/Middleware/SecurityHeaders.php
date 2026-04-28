97eb908cedf2e961df879aa5c3d9806d67903ca1
Emam Hosen <emamhsajeeb@gmail.com>
2026-04-28 16:52:59 +0600

    Update SecurityHeaders.php

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -22,11 +22,11 @@ class SecurityHeaders
         // Content Security Policy
         $response->headers->set('Content-Security-Policy',
             "default-src 'self'; ".
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com; ".
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com https://challenges.cloudflare.com https://static.cloudflareinsights.com https://fonts.googleapis.com https://fonts.gstatic.com https://translate.google.com http://localhost:5173 https://aero-enterprise-suite.test:5173; ".
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; ".
             "font-src 'self' https://fonts.gstatic.com; ".
             "img-src 'self' data: https:; ".
-            "connect-src 'self' https://api.erp.dhakabypass.com; ".
+            "connect-src 'self' https://api.erp.dhakabypass.com https://challenges.cloudflare.com; ".
             "frame-ancestors 'none';"
         );
 
